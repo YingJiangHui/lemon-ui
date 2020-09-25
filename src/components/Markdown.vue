3e05bbc976ea8d7266671d8b5f2d9eb4c1d61797
dark <473380917@qq.com>
2020-09-25 17:50:53 +0800

    优化代码Markdown组件

---
+++ src/components/Markdown.vue
@@ -0,0 +1,28 @@
+<template>
+<article v-html="content" class="markdown-body"></article>
+</template>
+
+<script lang="ts">
+import {
+    ref
+} from 'vue'
+export default {
+    props: {
+        path: {
+            type: String,
+            required: true
+        }
+    },
+    setup(props) {
+        const content = ref < string > (null)
+        import(props.path).then((data) => {
+            content.value = data.default
+        }).catch((err) => {
+            console.log(err)
+        })
+        return {
+            content
+        }
+    }
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,11 @@ import TabsDemo from './components/TabsDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Install from './views/Install.vue'
-import Intro from './views/Intro.vue'
-import getStarted from './views/GetStarted.vue'
+import Markdown from './components/Markdown.vue'
+import { h } from 'vue'
+const md = (filename)=>{
+    return {path:`../markdown/${filename}.md`,key:filename}
+}
 const hashHistory = createWebHashHistory()
 export const router = createRouter({
     history:hashHistory,
@@ -16,9 +18,9 @@ export const router = createRouter({
         {path:'/',component:Home},
         {path:'/doc',component:Doc,children:[
             {path:'',component:DocDemo},
-            {path:'intro',component:Intro},
-            {path:'install',component:Install},
-            {path:'get-started',component:getStarted},
+            {path:'intro',component:h(Markdown,md('intro'))},
+            {path:'install',component:h(Markdown,md('install'))},
+            {path:'get-started',component:h(Markdown,md('get-started'))},
             {path:'switch',component:SwitchDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
